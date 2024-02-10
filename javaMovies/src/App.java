3a6221ea8b332c455643d91963926d5bc6508817
jessochs <fac17002@byui.edu>
2024-02-10 16:46:39 -0700

    changed some functionality

--- javaMovies/src/App.java
+++ javaMovies/src/App.java
@@ -4,13 +4,6 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        Fantasy fant1 = new Fantasy();
-        Action act1 = new Action();
-        Romcom rom1 = new Romcom();
-        Pixar pixar1 = new Pixar();
-        Comedy comedy1 = new Comedy();
-        // instantiate one of each class
-
         System.out.println("Hello, Welcome to the movie generator!");
         System.out.println("");
         Scanner scan = new Scanner(System.in);
@@ -35,23 +28,32 @@ public class App {
 
             switch(response) {
                 case 1:
+                    Comedy comedy1 = new Comedy();
+                    // the class is instantiated here so if the same option is chosen 
+                    // twice it won't keep giving the same answer everytime.
+                    // You will still get repeat movies since there aren't 
+                    // man option in the array, but it should change
                     comedy1.DisplayComedyMovie();
                     System.out.println("");
                     break;
                 case 2:
+                    Fantasy fant1 = new Fantasy();
                     fant1.DisplayFanstasyMovie();
                     System.out.println("");
                     break;
                 case 3:
+                    Action act1 = new Action();
                     act1.DisplayActionMovie();
                     System.out.println("");
                     break;
                 case 4:
+                    Romcom rom1 = new Romcom();
                     rom1.DisplayRomCom();
                     
                     System.out.println("");
                     break;
                 case 5:
+                    Pixar pixar1 = new Pixar();
                     pixar1.DisplayPixarMovie();
                     System.out.println("");
                     break;
